d37d102e33bb601a992dda92c28aa137daed6db7
Tranita Terry <tranitacharterry@icloud.com>
2020-10-01 20:28:19 -0500

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by MXC Swift  on 10/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
